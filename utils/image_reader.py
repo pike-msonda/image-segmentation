32b1086f96c7e1f9ac24e0016eb18959895f1340
RenoGunray <pike.msonda@gmail.com>
2019-02-13 13:25:24 +0300

    updating evaluation procedure

--- utils/image_reader.py
+++ utils/image_reader.py
@@ -5,8 +5,6 @@ class ImageReader:
 
     def __init__(self, folder=None):
         self.folder = folder
-        # self.width = width
-        # self.height = height
 
     def read(self):
         folder = self.folder + "/"
@@ -19,12 +17,8 @@ class ImageReader:
             print(path)
             list_img_name.append(path.split('/')[1])
             img = cv2.imread(path)
-            print(img.shape)
             self.width = img.shape[0]
             self.height = img.shape[1]
-            # import pdb; pdb.set_trace()
-            # if self.width is not None and self.height is not None:
-            #     img = cv2.resize(img, (self.width, self.height),interpolation=cv2.INTER_AREA)
             rgb_img = img.reshape((img.shape[0] * img.shape[1], 3))
             list_img.append(rgb_img)
         return list_img, list_img_name
